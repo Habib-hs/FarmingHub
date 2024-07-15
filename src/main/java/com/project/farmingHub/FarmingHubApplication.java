42befd4d680c2d519ffbc156595b13895980d8cf
Habib-hs <mhabibullahhs@gmail.com>
2024-07-15 08:40:20 +0600

    first commit

---
+++ src/main/java/com/project/farmingHub/FarmingHubApplication.java
@@ -0,0 +1,13 @@
+package com.project.farmingHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmingHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmingHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/farmingHub/FarmingHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.farmingHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmingHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
